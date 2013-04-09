1c9e6f6357b9e721ed344c01b284b97c73c886d9
Sean Chen <sean.chen@leocorn.com>
2013-04-09 13:24:36 -0400

    the commit page template as a sample.

---
+++ wp-gitweb/templates/page-commit.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * Template Name: WP GitWeb Commit
+ * Description: a page template to show details changeset for a
+ * git commit.
+ */
+?>
+
+<?php
+get_header();
+wp_enqueue_script('jquery-ui-dialog');
+wp_enqueue_style('jquery-ui');
+
+$commit = wpg_get_request_param('id');
+?>
+
+<div id="right_column">
+  <?php echo wpg_widget_changeset_view($commit); ?>
+</div>
+
+<?php get_footer(); ?>
+
