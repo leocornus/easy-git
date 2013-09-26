0828549fd07fa1331bb6aa78dc5957c27ef89765
Sean Chen <sean.chen@leocorn.com>
2013-09-26 13:12:54 -0400

    update the version to 0.2, get ready to release.

--- wp-gitweb/wp-gitweb.php
+++ wp-gitweb/wp-gitweb.php
@@ -3,7 +3,7 @@
 Plugin Name: WP-GitWeb
 Plugin URI: http://www.github.com/leocornus/easy-git
 Description: GitWeb Interface for WordPress
-Version: 0.1
+Version: 0.2
 Author: Leocornus Ltd.
 Author URI: http://www.leocorn.com
 License: GPLv2
